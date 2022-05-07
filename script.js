2c81854a9c178f68a9b15e68fd65285ea145191b
KamilaDilmurat <89537254+KamilaDilmurat@users.noreply.github.com>
2022-05-06 19:37:37 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,49 +1,109 @@
 require([
-        "esri/WebScene",
-        "esri/views/SceneView",
-        "esri/layers/Layer"
-      ], function (Map, SceneView, Layer) {
-        const map = new Map({
-          basemap: "dark-gray",
-          ground: "world-elevation" 
-        });
+      "esri/WebScene",
+      "esri/views/SceneView",
+      "esri/Camera",
+      "esri/widgets/Home",
+      "dojo/domReady!"
+    ], function(WebScene, SceneView, Camera, Home) {
 
-       var view = new SceneView({
-          map: map,
-          container: "viewDiv",
-         scale: 500000,
-          center: [87.5, 43.8]
-        });
-
-        var layer = Layer.fromPortalItem({
-          portalItem: {
-            id: "001215337f694fb4a44264582520d507"
-          }
-        })
-          .then(addLayer)
-          .catch(rejection);
-        
-         var layer = Layer.fromPortalItem({
-          portalItem: {
-            id: "29148795c78c4591a43902f90680fe82"
-          }
-        })
-          .then(addLayer)
-          .catch(rejection);
-  
-  var layer = Layer.fromPortalItem({
-          portalItem: {
-            id: "1930a11f49ae402bb725398c8a3c79e9"
-          }
-        })
-          .then(addLayer)
-          .catch(rejection);
-
-        // Adds the layer to the map once it loads
-        function addLayer(layer) {
-          map.add(layer);
+    
+      /*var map = new Map({
+        basemap: "dark-gray",
+        ground: "world-elevation"
+      });*/
+      var scene = new WebScene({
+        portalItem:{
+         id:"001215337f694fb4a44264582520d507" 
         }
-        function rejection(error) {
-          console.log("Layer failed to load: ", error);
-        }
+      });
+      
+      var camera = new Camera({
+        position: [
+          87.5980, // lon
+          43.8295, // lat
+          5000// elevation in meters
+        ],
+        tilt:0,
+        heading: 0
       })
+      
+      var camera2 = new Camera({
+        position: {
+          x: 116.3941,
+          y: 39.9308,
+          z: 5000
+        },
+        tilt: 80,
+        heading: 270
+      })
+      
+      var camera3 = new Camera({
+        position: {
+          x: -90.2194,
+          y: 36.6427,
+          z: 5000
+        },
+        tilt: 80,
+        heading:270
+      });
+
+      var view = new SceneView({
+        container: "viewDiv",
+        map: scene,
+        viewingMode:"global",
+        camera: camera,
+        environment: {
+            lighting: {
+              date: new Date(),
+              directShadowsEnabled: true,
+              // don't update the view time when user pans.
+              // The clock widget drives the time
+              cameraTrackingEnabled: false
+            }
+        },
+    });
+    
+    var homeBtn = new Home({
+        view: view
+      });
+
+      // Add the home button to the top left corner of the view
+    view.ui.add(homeBtn, "top-left");
+    
+    [v1,v2,v3].forEach(function(button) {
+      button.style.display = 'flex';
+      view.ui.add(button, 'top-right');
+    });
+      
+      
+   
+    v1.addEventListener('click', function() {
+      // reuse the default camera position already established in the homeBtn
+      view.goTo({
+        target:camera2
+      });
+    });
+    
+    v2.addEventListener('click', function() {
+      // reuse the default camera position already established in the homeBtn
+      view.goTo({
+        target:camera
+      });
+    });
+      
+      v3.addEventListener('click', function() {
+      // reuse the default camera position already established in the homeBtn
+      view.goTo({
+       position: {
+          x: -71.0096,
+          y: 42.3675,
+          z: 5000
+        },
+        tilt: 80,
+        heading:270
+      });
+    });
+
+
+    });
+
