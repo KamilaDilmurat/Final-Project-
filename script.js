7063e5ff74e7404e77c5df7a6a1d136d368f3bc3
KamilaDilmurat <89537254+KamilaDilmurat@users.noreply.github.com>
2022-05-06 19:53:31 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,109 +1,97 @@
-require([
-      "esri/WebScene",
-      "esri/views/SceneView",
-      "esri/Camera",
-      "esri/widgets/Home",
-      "dojo/domReady!"
-    ], function(WebScene, SceneView, Camera, Home) {
 
-    
-      /*var map = new Map({
-        basemap: "dark-gray",
-        ground: "world-elevation"
-      });*/
-      var scene = new WebScene({
-        portalItem:{
-         id:"001215337f694fb4a44264582520d507" 
+
+require([
+        "esri/WebScene",
+        "esri/views/SceneView",
+        "esri/layers/Layer"
+      ], function (Map, SceneView, Layer) {
+        const map = new Map({
+          basemap: "dark-gray",
+          ground: "world-elevation" 
+        });
+
+       var view = new SceneView({
+          map: map,
+          container: "viewDiv",
+         scale: 500000,
+          center: [87.5, 43.8]
+        });
+
+        var layer = Layer.fromPortalItem({
+          portalItem: {
+            id: "001215337f694fb4a44264582520d507"
+          }
+        })
+          .then(addLayer)
+          .catch(rejection);
+  
+  
+
+        // Adds the layer to the map once it loads
+        function addLayer(layer) {
+          map.add(layer);
+        }
+        function rejection(error) {
+          console.log("Layer failed to load: ", error);
         }
       });
-      
-      var camera = new Camera({
+  
+var camera = new Camera({
         position: [
-          87.5980, // lon
-          43.8295, // lat
-          5000// elevation in meters
+          -90.1994, // lon
+          38, // lat
+          10000000// elevation in meters
         ],
         tilt:0,
         heading: 0
-      })
-      
-      var camera2 = new Camera({
-        position: {
-          x: 116.3941,
-          y: 39.9308,
-          z: 5000
-        },
-        tilt: 80,
-        heading: 270
-      })
-      
-      var camera3 = new Camera({
-        position: {
-          x: -90.2194,
-          y: 36.6427,
-          z: 5000
-        },
-        tilt: 80,
-        heading:270
       });
 
-      var view = new SceneView({
-        container: "viewDiv",
-        map: scene,
-        viewingMode:"global",
-        camera: camera,
-        environment: {
-            lighting: {
-              date: new Date(),
-              directShadowsEnabled: true,
-              // don't update the view time when user pans.
-              // The clock widget drives the time
-              cameraTrackingEnabled: false
-            }
-        },
-    });
-    
-    var homeBtn = new Home({
+
+var homeBtn = new Home({
         view: view
       });
 
       // Add the home button to the top left corner of the view
     view.ui.add(homeBtn, "top-left");
     
-    [v1,v2,v3].forEach(function(button) {
-      button.style.display = 'flex';
-      view.ui.add(button, 'top-right');
-    });
-      
-      
-   
-    v1.addEventListener('click', function() {
-      // reuse the default camera position already established in the homeBtn
-      view.goTo({
-        target:camera2
-      });
+    [ber, bei,stl].forEach(function(button) {
+      button.style.display = '.esri-button';
+      view.ui.add(button, 'bottom-right');
     });
     
-    v2.addEventListener('click', function() {
+
+ber.addEventListener('click', function() {
+      // reuse the default camera position already established in the homeBtn
+      view.goTo({
+        position: {
+          x: 13.36,
+          y: 52.56,
+          z: 5000000
+        },
+        tilt: 0,
+        heading: 0
+      });
+    });
+
+    bei.addEventListener('click', function() {
+      // reuse the default camera position already established in the homeBtn
+      view.goTo({
+        position: {
+          x: 116.4074,
+          y: 39.9042,
+          z: 5000000
+        },
+        tilt: 0,
+        heading: 0
+      });
+    });
+      
+      
+   stl.addEventListener('click', function() {
       // reuse the default camera position already established in the homeBtn
       view.goTo({
         target:camera
       });
     });
-      
-      v3.addEventListener('click', function() {
-      // reuse the default camera position already established in the homeBtn
-      view.goTo({
-       position: {
-          x: -71.0096,
-          y: 42.3675,
-          z: 5000
-        },
-        tilt: 80,
-        heading:270
-      });
-    });
-
-
-    });
-
+ 
+    
